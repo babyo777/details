3bcae67abc90f4d25b5e7d61cb1996832ab2413a
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-19 06:55:34 +0530

    cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { Router } from "./routes/index.js";
+import cors from "cors";
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
-
+app.use(cors())
 app.use(Router)
 app.use((req,res)=>{
     res.json("Page not Found")
