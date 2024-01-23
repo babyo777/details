8eb6533dc4ff1e4430c78ea4321578a3c6a0fb3a
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-23 07:05:04 +0530

    {     "name": "adas",     "email": "ddasdsad",     "phone": "ddddasdadas" }

--- services/addData.js
+++ services/addData.js
@@ -3,6 +3,7 @@ import { addDoc, serverTimestamp } from "firebase/firestore";
 
 const addFormData = async (data, res) => {
   try {
+    if(Object.keys(data).length == 0) throw new Error("data is empty")
     const newData = {...data,date:serverTimestamp()}
     await addDoc(ContactRef, newData);
     res.status(200).json("success");
