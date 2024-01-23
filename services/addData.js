30e26c987b52b7f40d8dfef04ec855f57a5d2e76
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-23 06:25:58 +0530

    das

--- services/addData.js
+++ services/addData.js
@@ -2,6 +2,7 @@ import { ContactRef } from "./details.js";
 import { addDoc, serverTimestamp } from "firebase/firestore";
 
 const addFormData = async (data, res) => {
+  if(isFieldBlank(data.name)||isFieldBlank(data.email)||isFieldBlank(data.phone)) return   res.status(400).json("All fields required");
   try {
     const newData = {...data,date:serverTimestamp()}
     await addDoc(ContactRef, newData);
@@ -10,5 +11,9 @@ const addFormData = async (data, res) => {
     res.status(500).json(error.message);
   }
 };
+
+function isFieldBlank(value) {
+  return typeof value !== 'string' || value.trim() === '';
+}
 
 export { addFormData };
