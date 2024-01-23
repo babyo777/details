f58f0f765143fffebc0782c76c3baf7e59b474ed
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-23 06:19:47 +0530

    update

--- services/details.js
+++ services/details.js
@@ -38,7 +38,7 @@ const getDetails = async (username, res) => {
     };
     res.status(200).json(userData);
   } catch (err) {
-    res.status(500).json(err.message);
+    res.status(500).json("Access denied");
   }
 };
 
