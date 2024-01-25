ef347927cca83ebd879a624cce05b4675379dc4c
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-25 21:07:22 +0530

    fixed

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,17 +2,17 @@ import { configDotenv } from "dotenv"
 
 configDotenv()
 const config = {
-    "type": process.env.type,
-    "project_id": process.env.project_id,
-    "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": process.env.auth_uri,
-    "token_uri": process.env.token_uri,
-    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-    "client_x509_cert_url": process.env.client_x509_cert_url,
-    "universe_domain": process.env.universe_domain
+    "type": "service_account",
+    "project_id": "napster-2f2b4",
+    "private_key_id": "35903002c11cd3b70f5a43f5f752a7615dfa2d8e",
+    "private_key": "-----BEGIN PRIVATE KEY-----MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDJVJFcSyWjQSyFX8MYaXzUzo9zMJCxUYTXT44ndtzx65hSEuMNRHnd7B1290oJPMs2yHS+Rlhy8AvQOFJQLZgYnDOOLIQlNX1KHFf7+DSUEx9+MBGorVe30qTXiOxI1r7zKUu8ujwojQs0 fc1JgFMN/cPPP5UXRKCaYWRl6HZG/WqVNhl+1SW2UAMpiUJZ4+TR/TsxKlGPX2ou8m1t+pHPRx3yZFuHaYHqg5tG85RbvoMdM1raQ0QYadT4GoJClKJ+Mz6yN4+W5SmbQNbURqgjyXgY6P1sFHWG5LmvEVOuCcMTRdRFjtywvMvxs9EFGWnzeXv0RdmeCvLeFh9M2qexAgMBAAECggEAC6S36WSxLwwFtUrwqgNDxGeTypTMRsJQ/UYXvGGL7UkeRzTaFmTXlXA301msLvTp8xJMaiAAXS6pHZLRnoXHLjkuZB0KoWpCrjFjHJU3WTmdOyYwMeygeObrlfQlvMqqmBPTJ6gvnXrBY05D5pR6I9eBZlrQ1lvSMUt2S8x8tqXhKINCg5yQ4sSHyKFsH/FAwdcrvS1WjbQoAZ4rTFyJPg8jHuAZmVqPZZKN+DzZbOrLi/JWl0zhyWRWXafy+TTcPIWv7IKOZYjw7nqFCc5LaXzNiuIZVuXlNAIH0hy26orqyJVeSNu+QYxN69iiYevQe301gu3scNgAIDnHJ3PaXwKBgQD/asX3+15bawPJtxl1Eq8e0VS3Z6uSDVqF4tagG1oxpfR9HVnnpflg+VHZfR5v2c5h4VRdiQA+kMPFuAqXjMhnxAShkp0wS4gpiNkSc+QM+sr8R4TrSiSOsnoXkANXiy18AfZ+L5lq71UN5hQwY83NbvNXH8eHjo7xWIEEJSPbTwKBgQDJyjHJZp4ff/bvtnYZk8wRaILDl4fLZ2d32XQTToR8CgCKNFFxOh9rjqQPTAjKdzqQm4gAqae4/wXdCyeSGv/+bi26Y9425bukTwGStdPOF1brD25YSaL8U4c/2Pnmb/vJeJK99pJib/GtzWa31rKNwvGJmz5jb09FmfEkTYyM/wKBgQDmP5OdG/4QfSUaBzQsxJ4XMu+ishMJg34yKa3/AQlKrmb7DqWIXKe1UqwFWnRAsmEI/X73HtSYkhnZuDHcMm5KlHr0SF3pNn3Vq2a76rsPNFoeGD7reJgCB0Rmz00j8vQa0Q4q7K/ppTzQXbOfd6kD41v8E0rkmY+kY3tSiESNowKBgGSG52CnT7TvogxvwMwjNjGh+CsKrWrmu8QYP9xybEwDC5vOTphy1T5XFGJOhb6fwTxTT2DNJDJkUf551R/TdZ7CMuO9ZvHAZ0eG2IjHLppoRDPcPyOao6jC0Ak9xdlIBo6rE6exU+HYVPU586c4PUGkUYI1JdKRnFNXSqvEOnXvAoGBAJjZrvHKovyu6/k1KT3hvgMRig4N/dozu4jUnwQnUrCkp+hl1O3wZLuonUT4nVQ/nKrWKaKTfy1zamp72147+yV84/CiQwCNf73OctVeJpFVmOUPz/bdxfodGCBNm5lH/I5PsPIfMCIVDIAfWY/JvDT3HR1RqHmyguWzcgv6fbhk-----END PRIVATE KEY-----",
+    "client_email": "firebase-adminsdk-spckv@napster-2f2b4.iam.gserviceaccount.com",
+    "client_id": "105397549173403974159",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-spckv%40napster-2f2b4.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
 }
 
 export {config}
--- services/addData.js
+++ services/addData.js
@@ -2,15 +2,18 @@ import { ContactRef, firebaseAdmin ,admin} from "./adminfirebase.js";
 
 const addFormData = async (data, res) => {
   if(!data.token) return res.status(402).json("Token not provided");
-  const user = await firebaseAdmin.auth().verifyIdToken(data.token);
   try {
-    if (!user.email_verified) throw new Error("invalid token");
-    if (Object.keys(data).length == 0) throw new Error("Fields is empty");
+    const user = await firebaseAdmin.auth().verifyIdToken(data.token);
+    if (!user.email_verified) return  res.status(403).json("access denied");
+    if (Object.keys(data).length == 0) return res.status(402).json("Fields is empty");
     const {token,...rest} =data
     const newData = { ...rest, date:admin.firestore.FieldValue.serverTimestamp() };
     ContactRef.add(newData)
     res.status(200).json("success");
   } catch (error) {
+    if (error.code === "auth/argument-error") {
+      return res.status(403).json("Access denied - Token not found");
+    }
     console.log(error.message);
     res.status(500).json(error.message);
   }
