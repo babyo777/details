e868117b2b74401ffcfba91804a7039fc0a8371a
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-08 11:31:13 +0530

    up

--- Utils/insta-details.js
+++ Utils/insta-details.js
@@ -1,24 +1,28 @@
-import axios from "axios"
-import cheerio from "cheerio"
+import axios from "axios";
+import cheerio from "cheerio";
 
-async function scrapeInstagramProfile(username,res){
-try {
-  const data = await Promise.all(username.map(async username=>{
-        const url = `https://www.instagram.com/${username}`
-        const response = await axios.get(url)
-        const $ = cheerio.load(response.data)
+async function scrapeInstagramProfile(username, res) {
+  try {
+    const data = await Promise.all(
+      username.map(async (username) => {
+        const url = `https://www.instagram.com/${username}/?_a=1`;
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
         const data = {
-            username:username,
-            fname:$('meta[property="og:title"]').attr("content")?.replace(` (@${username}) • Instagram photos and videos`,""),
-            dp:$('meta[property="og:image"]').attr("content"),
-            url:url
-        }
-        return data
-    }))
+          username: username,
+          fname: $('meta[property="og:title"]')
+            .attr("content")
+            ?.replace(` (@${username}) • Instagram photos and videos`, ""),
+          dp: $('meta[property="og:image"]').attr("content"),
+          url: url,
+        };
+        return data;
+      })
+    );
 
-    res.status(200).json(data)
-} catch (error) {
-    res.status(500).json(error.message)
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 }
-}
-export {scrapeInstagramProfile}
+export { scrapeInstagramProfile };
